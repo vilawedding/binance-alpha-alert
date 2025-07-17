88ba2a319240895a9020de689687363a7a93d152
nguyenvietvinh <nguyenvietvinh@cryptopie-labo.com>
2025-07-17 15:23:37 +0700

    remove account.json

--- slack.js
+++ slack.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
+const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
 export async function sendSlackMessage(message) {
   const url = SLACK_WEBHOOK_URL
   if (!url) {
